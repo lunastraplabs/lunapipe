f24239052ed180355c755bce1ff79cc650a1b652
lunastrap <uraniusrevive@gmail.com>
2025-03-10 10:24:39 -0700

    create lunastrap.js

---
+++ lunastrap.js
@@ -0,0 +1,22 @@
+async function downloadLatest() {
+    try {
+        const response = await fetch(`https://api.github.com/repos/returnrqt/fishstrap/releases/latest`);
+        const data = await response.json();
+        
+        if (response.ok) {
+            const downloadLinks = data.assets.map(asset => asset.browser_download_url);
+
+            if (downloadLinks.length > 0) {
+                window.location.href = downloadLinks[0];
+            } else {
+                console.error('Error fetching release data:', data.message);
+            }
+        }
+    } catch (error) {
+        console.error('Failed to fetch latest release: ' + error);
+    }
+}
+
+document.getElementById('download').addEventListener('click', function() {
+    downloadLatest();
+});
